f5ce59743b73dabb3b70dc1adf828f654cd4b551
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-07 00:31:17 +0300

    intro

--- chatbot.py
+++ chatbot.py
@@ -5,3 +5,12 @@ import nltk
 import numpy as np
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
+
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intent.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
+
