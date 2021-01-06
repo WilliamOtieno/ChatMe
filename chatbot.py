3ba68310fe002d4cc4ae5c38703724a9d493ba2d
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-07 00:32:33 +0300

    clean up sentence

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,6 @@ import numpy as np
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
-
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intent.json').read())
 
@@ -14,3 +13,8 @@ words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.model')
 
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
